bbd17d512e97303f8eff2a6a4b9fcdef96d49b13
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-11-23 17:24:12 +0100

    ADD file function.php

---
+++ function.php
@@ -0,0 +1,53 @@
+<?php 
+
+//Lunghezza password
+$password_length = isset($_GET['password_length']) ? (int)$_GET['password_length'] : 0;
+var_dump($password_length);
+
+// Funzione per creare random index di un array
+function randomIndex($array){
+    $min = 0;
+    $max = count($array) - 1; 
+    $random_index = rand($min, $max);
+    return $random_index;
+}
+
+// Funzione restituisce un elemento random di un array
+function getRandomElementArray($array) {
+    $index = randomIndex($array);
+    $element = $array[$index];
+    return $element;
+};
+
+// Funzione crea password in base alla lunghezza richiesta
+function getPassword($password_length){
+    // I dati da uttilizare per creazione del password
+    $uppercase_letter = range('A','Z');
+    $lowercase_letter = range('a','z');
+    $array_number = range('0','9');
+    $array_symbols = str_split("'[@_!#$%^&*()<>?/|}{~:]']");
+        
+    // Array con tutti i charateri da utilizzare per creare password
+    $array_charters = [
+        $uppercase_letter,
+        $lowercase_letter,
+        $array_number,
+        $array_symbols
+    ];
+
+    $password = [];
+    // Ciclo per assegnare un charattere diverso con ogni giro
+    for($i = 0; $i < $password_length; $i++){
+        $index_type_charter = randomIndex($array_charters);
+        // $charter = $array_charters[$index_type_charter][rand(0, count($array_charters[$index_type_charter]) - 1)];
+        $charter = getRandomElementArray($array_charters[$index_type_charter]);
+        array_push($password, $charter);
+    }
+
+    return $password;
+};
+
+// Password
+$password = implode("", getPassword($password_length));
+
+?>
--- index.php
+++ index.php
@@ -1,54 +1,6 @@
 <?php
 
-//Lunghezza password
-$password_length = isset($_GET['password_length']) ? (int)$_GET['password_length'] : 0;
-var_dump($password_length);
-
-// Funzione per creare random index di un array
-function randomIndex($array){
-    $min = 0;
-    $max = count($array) - 1; 
-    $random_index = rand($min, $max);
-    return $random_index;
-}
-
-// Funzione restituisce un elemento random di un array
-function getRandomElementArray($array) {
-    $index = randomIndex($array);
-    $element = $array[$index];
-    return $element;
-};
-
-// Funzione crea password in base alla lunghezza richiesta
-function getPassword($password_length){
-    // I dati da uttilizare per creazione del password
-    $uppercase_letter = range('A','Z');
-    $lowercase_letter = range('a','z');
-    $array_number = range('0','9');
-    $array_symbols = str_split("'[@_!#$%^&*()<>?/|}{~:]']");
-        
-    // Array con tutti i charateri da utilizzare per creare password
-    $array_charters = [
-        $uppercase_letter,
-        $lowercase_letter,
-        $array_number,
-        $array_symbols
-    ];
-
-    $password = [];
-    // Ciclo per assegnare un charattere diverso con ogni giro
-    for($i = 0; $i < $password_length; $i++){
-        $index_type_charter = randomIndex($array_charters);
-        // $charter = $array_charters[$index_type_charter][rand(0, count($array_charters[$index_type_charter]) - 1)];
-        $charter = getRandomElementArray($array_charters[$index_type_charter]);
-        array_push($password, $charter);
-    }
-
-    return $password;
-};
-
-// Password
-$password = implode("", getPassword($password_length));
+include __DIR__ . '/function.php';
 
 ?>
 <!DOCTYPE html>
@@ -65,7 +17,7 @@ $password = implode("", getPassword($password_length));
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h1>Password: <?php print_r($password)?></h1>
+                    <h1>Password: <?php echo $password?></h1>
                     <form action="" method="GET">
                         <label>Choose a password:</label>
                         <select name="password_length">
