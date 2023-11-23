941fa0c5a331148d5e016244971720d741b0fa41
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-11-23 15:33:36 +0100

    ADD Form

--- index.php
+++ index.php
@@ -8,6 +8,26 @@
 </head>
 <body>
 
+    <section class="get-password">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Password</h1>
+                    <form action="" method="GET">
+                        <label for="cars">Choose a password:</label>
+                        <select name="length_password">
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                        </select>   
+                        <br><br>
+                        <input type="submit" value="Submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
