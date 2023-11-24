da31a29e56e13e0debf18545ce6ba3ee735f9d80
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-11-24 14:03:50 +0100

    ADD function to manage the checkbox

--- bonus/app.js
+++ bonus/app.js
@@ -1,3 +1,30 @@
+ const checkboxUppercaseLetterDOMElement = document.getElementById('has_uppercase_letter');
+ const checkboxLowercaseLetterDOMElement = document.getElementById('has_lowercase_letter');
+ const checkboxNumberDOMElement = document.getElementById('has_number');
+ const checkboxSymbolsDOMElement = document.getElementById('has_symbols');
+
+function check(DOMElemnt) {
+    DOMElemnt.checked = true;
+};
+
+function uncheck(DOMElemnt) {
+    DOMElemnt.checked = false;
+};
+
+function onChangeChecked() {
+    isCheckUpper = checkboxUppercaseLetterDOMElement.checked;
+    isCheckLower = checkboxLowercaseLetterDOMElement.checked;
+    isCheckNumber = checkboxNumberDOMElement.checked;
+    isCheckSymbols = checkboxSymbolsDOMElement.checked;
+
+    if (!isCheckUpper && !isCheckLower && !isCheckNumber && !isCheckSymbols) {
+        check(checkboxUppercaseLetterDOMElement);
+        check(checkboxLowercaseLetterDOMElement);
+        check(checkboxNumberDOMElement);
+        check(checkboxSymbolsDOMElement); 
+    }
+};
+
 function updateValue(event) {
     document.getElementById("rate_val").innerText = event.value;
-  }
+};
--- bonus/index.php
+++ bonus/index.php
@@ -42,24 +42,20 @@ if ($password) {
                                 <p><?php echo $password?></p>
                                 <ul style="list-style: none; padding: 0;">
                                     <li>
-                                        <input id="has_uppercase_letter" type="checkbox" name="has_uppercase_letter">
+                                        <input onchange=onChangeChecked() checked id="has_uppercase_letter" type="checkbox" name="has_uppercase_letter">
                                         <label for="has_uppercase_letter" class="form-label">Uppercase letter</label>
                                     </li>
                                     <li>
-                                        <input id="has_lowercase_letter" type="checkbox" name="has_lowercase_letter">
+                                        <input onchange=onChangeChecked() checked id="has_lowercase_letter" type="checkbox" name="has_lowercase_letter">
                                         <label for="has_lowercase_letter" class="form-label">Lowercase letter</label>
                                     </li>
                                     <li>
-                                        <input id="has_number" type="checkbox" name="has_number">
+                                        <input onchange=onChangeChecked() checked id="has_number" type="checkbox" name="has_number">
                                         <label for="has_number" class="form-label">Number</label>
                                     </li>
                                     <li>
-                                        <input id="has_symbols" type="checkbox" name="has_symbols">
+                                        <input onchange=onChangeChecked() checked id="has_symbols" type="checkbox" name="has_symbols">
                                         <label for="has_symbols" class="form-label">Symbols</label>
-                                    </li>
-                                    <li>
-                                        <input id="has_all" type="checkbox" name="has_all">
-                                        <label for="has_all" class="form-label">ALL</label>
                                     </li>
                                 </ul>
                                 <input type="submit" value="Genera" class="btn btn-primary">
@@ -70,10 +66,6 @@ if ($password) {
             </div>
         </div>
     </section>
-    
-    
-    
-
     <script src="app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
